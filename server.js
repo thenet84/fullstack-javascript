d588956082138228f3449b7dd0c0212df77dbc4f
diglesib <the.net.wo@gmail.com>
2018-07-20 23:08:37 +0100

    v2.3

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
-import config, {nodeEnv, logStart} from  './config'; 
+import config from  './config'; 
+import express from 'express';
+import apiRouter from './api';
 
-logStart('hello');
-console.log(config, nodeEnv);
+const server = express();
+
+server.get('/',(req, res) =>{
+  res.send('Hello Express!');
+});
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, ()=>{
+  console.info('Express listening on port ' + config.port);
+});
