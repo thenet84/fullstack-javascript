26db80ddba61822986f91599a1101dc5ae7fbbe4
diglesib <the.net.wo@gmail.com>
2018-07-24 01:33:45 +0100

    4.3

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import config from  './config'; 
 import express from 'express';
 import apiRouter from './api';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
 
 const server = express();
 
@@ -10,6 +12,11 @@ server.get('/',(req, res) =>{
     content: ''
   });
 });
+
+server.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public')
+}));
 
 server.use('/api', apiRouter);
 server.use(express.static('public'));
