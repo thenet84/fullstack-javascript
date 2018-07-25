88fc55238e1420783f5ea618bb1b4ea40bf77d6f
diglesib <the.net.wo@gmail.com>
2018-07-25 02:14:06 +0100

    v6.1

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Header from './Header';
-import ContestPreview from './ContestPreview';
+import ContestList from './ContestList';
 
 
 class App extends React.Component {
@@ -29,11 +29,7 @@ class App extends React.Component {
     return (
       <div>
         <Header message={this.state.pageHeader} />
-        <div>
-          {this.state.contests.map(contest =>
-            <ContestPreview key={contest.id} {...contest} />
-          )}
-        </div>
+        <ContestList contests={this.state.contests} />
       </div>
     );
   }
--- src/components/ContestPreview.js
+++ src/components/ContestPreview.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ContestPreview = (contest) => {
-  return (
-    <div className='ContestPreview'>
-      <div className='category-name'>
-        {contest.categoryName}
+class ContestPreview extends React.Component{
+  handleClick = () => {
+    console.log(this.props.contestName);
+  };
+  render(){
+    return (
+      <div className='ContestPreview' onClick={this.handleClick}>
+        <div className='category-name'>
+          {this.props.categoryName}
+        </div>
+        <div className='contest-name'>
+          {this.props.contestName}
+        </div>
       </div>
-      <div className='contest-name'>
-        {contest.contestName}
-      </div>
-    </div>
-  );
+    );
+  }
+}
+
+ContestPreview.propTypes = {
+  categoryName: PropTypes.string.isRequired,
+  contestName: PropTypes.string.isRequired
 };
 
 export default ContestPreview;
